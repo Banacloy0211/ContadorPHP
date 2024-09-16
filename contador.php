1b29827093444d32d3521f9496e765cb921d7af5
JosBan <banacloyjcarcelen@gmail.com>
2024-09-16 21:03:50 +0200

    v1 contador php

---
+++ contador.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for($i = 0; $i <= 100; $i++){
+            echo "$i ,";
+        }
+        echo '<br>';
+    
+    ?>
+
+    <?php
+        $num = 10;
+        while($num!=0){
+            echo("$num ,");
+            $num--;
+        }
+    ?>
+</body>
+</html>
